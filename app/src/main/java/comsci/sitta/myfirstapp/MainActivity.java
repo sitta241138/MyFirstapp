e1a80235362d4ba8a33b49c7252a0452a8d318c5
sitta241138 <sitta241138@hotmail.com>
2017-02-16 11:32:27 +0700

    Button MoreInfo Complete

--- app/src/main/java/comsci/sitta/myfirstapp/MainActivity.java
+++ app/src/main/java/comsci/sitta/myfirstapp/MainActivity.java
@@ -1,7 +1,10 @@
 package comsci.sitta.myfirstapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+    }// main methot onCreate
+
+    public void onClickMoreInfo(View view){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("https://www.dlt.go.th/th/dlt-knowledge/view.php?_did=111"));
+        startActivity(intent);
+    }// main methot onClickMoreInfo
+
+}// main class
