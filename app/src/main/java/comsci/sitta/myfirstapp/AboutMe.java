94f8b7aab861c3f1bc40b11368c75cfedd8e66db
sitta241138 <sitta241138@hotmail.com>
2017-03-16 20:27:27 +0700

    test up load github for check error

--- app/src/main/java/comsci/sitta/myfirstapp/AboutMe.java
+++ app/src/main/java/comsci/sitta/myfirstapp/AboutMe.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class AboutMe extends AppCompatActivity {
 
@@ -12,7 +13,19 @@ public class AboutMe extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
+
+        TextView gophone = (TextView) findViewById(R.id.phonenumber);
+        gophone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent phoneIntent = new Intent(Intent.ACTION_VIEW);
+                phoneIntent.setData(Uri.parse("tel:0970292720"));
+                startActivity(phoneIntent);
+            }
+        });
+
     }
+
 
 
     public void onFacebook(View view){
@@ -20,6 +33,7 @@ public class AboutMe extends AppCompatActivity {
         intent.setData(Uri.parse("https://www.facebook.com/profile.php?id=100001920246976"));
         startActivity(intent);
     }// main methot onClickMoreInfo
+
 
     public void btnBackAbout(View view){
         finish();
